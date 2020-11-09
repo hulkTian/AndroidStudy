eeb232d27e3be883659aa7574b2c73ec299e1d35
hulkTian <1509482122@qq.com>
2020-11-09 10:25:53 +0800

    添加笔记文件

---
+++ app/src/main/java/com/hulk/androidstudy/architecture_components/ArchitectureComponentsActivity.kt
@@ -0,0 +1,17 @@
+package com.hulk.androidstudy.architecture_components
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import com.hulk.androidstudy.R
+
+/**
+ * 架构组件
+ * Created by tzh on 2020/11/9.
+ */
+class ArchitectureComponentsActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_architecture_components)
+    }
+}
