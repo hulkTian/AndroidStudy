96f8e3b91b658ee78a07c848e39374129903084c
hulkTian <1509482122@qq.com>
2021-03-26 17:45:05 +0800

    update

--- common/src/main/java/com/hulk/common/base/BaseFragment.kt
+++ common/src/main/java/com/hulk/common/base/BaseFragment.kt
@@ -1,11 +1,7 @@
 package com.hulk.common.base
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 
 /**
